01cb033030fd275a54dd667adf6a16760126e695
Nick Nichols <nnichols@iwu.edu>
2013-01-15 16:35:10 -0600

    Moved problem selection and operator selection into seperate parameterized functions. Need to include sample chromosome creation and updated reporting.

--- JGAP-Code/MCP.java
+++ JGAP-Code/MCP.java
@@ -16,33 +16,96 @@ public class MCP{
 	/* Here we define the size of the test population and generations
 	 * to evolve for. These should be constant across trials for
 	 * fairness and to reduce the amount of tested variables.
+	 *
+	 * These are consistent with the upper bounds used by Eiben's work
 	 */
-	private static final int POPULATION_SIZE = 500;
-	private static final int GENERATIONS = 100;
+	private static final int POPULATION_SIZE = 200;
+	private static final int GENERATIONS = 500;
+	
+	// Problem characteristics
+	private static int diagCheck;
+	private static int fbCheck;
+	private static int esoCheck;
+	private static int mutCheck;
+	private static int problemNumber;
+	private static int dimensions;
+	private static int dimensionLength;
+	private static String filePath;
 		
 		
 	public static void main(String[] args) throws Exception{
-	
+			
 		// Set up a Configuration, which drives evolution
 		Configuration config = configurationSetup();
 		
+		// PULL DESCRIPTION FROM COMMAND LINE
+		getOperators( args );
 		
-		// Population size to evolve
-		config.setPopulationSize( POPULATION_SIZE );
+		/* Add the GeneticOperators
+		 * For testing, we will add the default set. Later, this function will
+		 * take a switching parameter to choose which operators we want
+		 */
 		
+		// Add diagonalization operator based on CLA
+		if( diagCheck > 0 ){
+			DiagonalizationOperator xOver = new DiagonalizationOperator( config );
+			xOver.setParents( diagCheck );
+			
+			if( problemNumber == 11 ){
+				xOver.setPermutation( true );
+			}
+			
+			config.addGeneticOperator( xOver );
+		}
 		
-		// Providing an arbitrary test amount for the MinimizingMakeChange problem
-		int changeTarget = 88;
+		// Add scanning operator based on CLA
+		if( fbCheck > 0 ){
+			FitnessBasedScanningOperator fbScan = new FitnessBasedScanningOperator( config );
+			fbScan.setParents( fbCheck );
+			
+			if( problemNumber == 11 ){
+				fbScan.setPermutation( true );
+			}
+			
+			config.addGeneticOperator( fbScan );
+		}
 		
+		// Add ESOs based on CLA
+		if( esoCheck != 0 ){
+			ElitistSchemaOverlayOperator eso = new ElitistSchemaOverlayOperator( config );
+			eso.setGenerations( GENERATIONS );
+			eso.setParents( esoCheck );
+			
+			// -1 means set to random k
+			if( esoCheck == -1 ){
+				eso.setMethod(1);
+			
+			// -2 means k is chosen by linear descent 
+			} else if( esoCheck == -2 ){
+				eso.setMethod(2);
+				
+			} else {
+				eso.setMethod(0);
+			}
+			
+			if( problemNumber == 11 ){
+				eso.setPermutation( true );
+			}
+				
+			config.addGeneticOperator( eso );
+		}
 		
-		// Import the fitness function and target it to our amount
-		// 		The FitnessFunction sub-class we pass along will contain problem details
-		FitnessFunction currentProblem = new MinimizingMakeChangeFitnessFunction( changeTarget );
-		
-		
-		// Pass the fitness function to our configuration
+		// Add mutation operator based on CLA
+		// Enables dynamic mutation rate, which is roughly 1 / CHROM LENGTH
+		// This matches theoretical results
+		if( mutCheck > 0 ){
+			MutationOperator mOperator = new MutationOperator( config );
+			config.addGeneticOperator( mOperator );
+		}
+
+		// Set up the fitness function
+		FitnessFunction currentProblem = getProblem( args, config );
 		config.setFitnessFunction( currentProblem );
-		
 		
 		/* To get the random initial population set up, we have to build
 		 * a sample Chromosome and give that to the Configuration. 
@@ -90,12 +153,12 @@ public class MCP{
 		
 		
 		// Report runtime values
-		report(fittest, endTime - startTime);
+		report(fittest, currentProblem, endTime - startTime);
 	}
 	
 	
 	// Output information about the run
-	public static void report( IChromosome fittest, long runTime ){
+	public static void report( IChromosome fittest, FitnessFunction problem, long runTime ){
 		
 		System.out.println( "The best solution contained the following: " );
 
@@ -129,6 +192,9 @@ public class MCP{
 	public static Configuration configurationSetup() throws Exception {
 		
 		Configuration config = new Configuration();
+				
+		// Population size to evolve
+		config.setPopulationSize( POPULATION_SIZE );
 		
 		// Set up a breeder to run the evolutions
 		config.setBreeder( new GABreeder() );
@@ -143,40 +209,154 @@ public class MCP{
 		config.setPreservFittestIndividual( true );
 		
 		// Allow for population shrinkage/growth
-		config.setKeepPopulationSizeConstant( true );
+		config.setKeepPopulationSizeConstant( false );
 		
 		// Set minimum thershold on population size
 		config.setMinimumPopSizePercent( 0 );
 		
-		// Set up the high evaluation on fitness functions
+		// Set up low-positive evaluation on fitness functions
+		// (Minimization tasks)
 		config.setFitnessEvaluator( new DefaultFitnessEvaluator() );
-		
-		// DeltaFitnessEvaluator should be used for low evaluation
 		
 		//Build the Chromosome Pool to store the population
 		config.setChromosomePool( new ChromosomePool() );
 		
-		
-		
-		/* Add the GeneticOperators and NaturalSelectors.
-		 * For testing, we will add the default set. Later, this function will
-		 * take a switching parameter to choose which operators we want
-		 */
-		 
 		// Add natural selector
 		// boolean switch means this is processed after genetic operators
 		StandardPostSelector nSelector = new StandardPostSelector( config );
 		config.addNaturalSelector( nSelector, false);
-		 
-		// Add crossover operator
-		DiagonalizationOperator xOver = new DiagonalizationOperator( config );
-		config.addGeneticOperator( xOver );
-		 
-		// Add mutation operator
-		MutationOperator mOperator = new MutationOperator( config );
-		config.addGeneticOperator( mOperator );
-		
-		
+				
 		return config;
+	}
+	
+	
+	// Get problem and setup information
+	public static void getOperators( String[] arguments ){
+		
+		// Is Diagonalization utilized?
+		// 0 - FALSE
+		// k - TRUE W/ K PARENTS
+		diagCheck = Integer.parseInt( arguments[0] );
+		
+		// Is FB-Scanning utilized?
+		// 0 - FALSE
+		// k - TRUE W/ K PARENTS
+		fbCheck = Integer.parseInt( arguments[1] );
+		
+		// Are Elitist Schema Overlays utilized?
+		// 0 - FALSE
+		// k - TRUE W/ K AS THE METHOD
+		//     -1 - RANDOM
+		//     -2 - LINEAR DESCENT
+		//     +k - CONSTANT K
+		esoCheck = Integer.parseInt( arguments[2] );
+		
+		// Is Mutation utilized?
+		// 0 - FALSE
+		// k - TRUE 
+		mutCheck = Integer.parseInt( arguments[3] );
+	}
+	
+	public static FitnessFunction getProblem( String[] arguments, Configuration config ){
+		
+		// PROBLEM # GUIDE 
+		// 1 - DeJong's Hyper-Sphere Function
+		// 2 - DeJong's Hyper-Ellipsoid Function
+		// 3 - Sum Of Powers Function
+		// 4 - Griewank Function
+		// 5 - Rastrigin Function
+		// 6 - Rosenbrock Function
+		// 7 - Michaelwicz Function
+		// 8 - Six-Hump Camel Function
+		// 9 - Schubert Function
+		// 10 - 1-0 Knapsack Problem
+		// 11 - Travelling Salesperson Problem
+		// 12 - NK-Landscape Minimization
+		problemNumber = Integer.parseInt( arguments[4] );
+		
+		
+		// Problem characteristics
+		// 1 - 6: int dimensions, int dimensionLength
+		// 7 - 9: int dimensionLength
+		// 10 - 11: String filePath
+		if( problemNumber >= 1 && problemNumber <= 6 ){
+			
+			dimensions = Integer.parseInt( arguments[5] );
+			dimensionLength = Integer.parseInt( arguments[6] );
+			
+		} else if( problemNumber >= 7 && problemNumber <= 9 ){
+			
+			dimensionLength = Integer.parseInt( arguments[5] );
+			
+		} else if(  problemNumber >= 10 && problemNumber <= 12 ){
+			
+			filePath = arguments[5];
+			
+		} else {
+			
+			System.err.println( "Invalid command line arguments" );
+			System.exit(0);
+			
+		}
+		
+		// Build the necessary fitness function for the problem
+		FitnessFunction currentProblem = null;
+		
+		switch( problemNumber ){
+			case 1:
+				currentProblem = new DeJongSphereFunction( dimensions, dimensionLength, true );
+				break;
+			
+			case 2:
+				currentProblem = new DeJongEllipsoidFunction( dimensions, dimensionLength, true );
+				break;
+				
+			case 3:
+				currentProblem = new SumOfPowersFunction( dimensions, dimensionLength, true );
+				break;
+				
+			case 4:
+				currentProblem = new GriewankFunction( dimensions, dimensionLength, true );
+				break;
+				
+			case 5:
+				currentProblem = new RastriginFunction( dimensions, dimensionLength, true );
+				break;
+				
+			case 6:
+				currentProblem = new RosenbrockFunction( dimensions, dimensionLength, true );
+				break;
+				
+			case 7:
+				currentProblem = new MichaelwiczFunction( dimensionLength, true );
+				break;
+				
+			case 8:
+				currentProblem = new CamelFunction( dimensionLength, true );
+				break;
+				
+			case 9:
+				currentProblem = new SchubertFunction( dimensionLength, true );
+				break;
+				
+			case 10:
+				currentProblem = new KnapsackFunction( filePath );
+				break;
+				
+			case 11:
+				currentProblem = new TSPFunction( filePath );
+				break;
+				
+			case 12:
+				currentProblem = new NKFunction( filePath );
+				break;
+				
+			default:
+				System.err.println("Invalid argument");
+				System.exit(0);
+				
+		}
+		
+		return currentProblem;
 	}
 }
